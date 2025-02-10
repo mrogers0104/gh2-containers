c34a553d49a0362007d415c735fbdbdf3799725e
Mark Rogers <mrogers@sabreindustries.com>
2025-02-10 11:39:41 -0600

    initial commit

---
+++ git-setup.bat
@@ -0,0 +1,11 @@
+REM
+REM
+REM
+
+echo "# gh2-containers" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+REM git branch -M main
+git remote add origin https://github.com/mrogers0104/gh2-containers.git
+git push -u origin main
